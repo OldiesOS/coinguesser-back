b1759a200d050faf2845c751f2d44dc15abdb4a8
minkui1826 <minkui1826@kyonggi.ac.kr>
2024-12-04 18:57:36 +0900

    데이터베이스 컨테이너화

---
+++ init.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE IF NOT EXISTS db;
+
+USE db;
+
+-- 유저 생성
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY '1234';
+
+-- 권한 부여
+GRANT ALL PRIVILEGES ON db.* TO 'admin'@'%';
+
+-- 변경사항 적용
+FLUSH PRIVILEGES;
+
+CREATE TABLE IF NOT EXISTS coin_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    coin VARCHAR(10) NOT NULL,
+    _time TIME NOT NULL,
+    real_value DECIMAL(10,4),
+    predicted_value DECIMAL(10,4) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS mobile_data (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    coin VARCHAR(10) NOT NULL,
+    _time TIME NOT NULL,
+    volume DECIMAL(10,2),
+    increase_rate DECIMAL(10,5)
+);
+
+
